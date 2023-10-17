4f1063a7dc6424812f6fc3268181fd75a9bb385d
Tomas <tomimoyanook@gmail.com>
2023-10-16 23:16:06 -0300

    animate

---
+++ app.js
@@ -0,0 +1,51 @@
+
+document.addEventListener("DOMContentLoaded",function(){
+    iniciarApp();
+});
+
+const iniciarApp = ()=>{
+    animacion();
+    animacionContacto();
+}
+
+
+
+const animacion =()=>{
+    const servicios = document.querySelector('.cuatroImgs');
+    const boxServiciosPrim = document.querySelector('.boxPrim')
+    const boxSecond = document.querySelector(".boxSecond")
+    const boxThrd = document.querySelector('.boxThrd')
+    const boxFour = document.querySelector('.boxFour')
+    window.addEventListener('scroll', function(){
+        if(servicios.getBoundingClientRect().top < 0){
+            boxServiciosPrim.classList.add("animate__zoomIn");
+            boxSecond.classList.add("animate__zoomIn");
+            boxThrd.classList.add("animate__zoomIn");
+            boxFour.classList.add("animate__zoomIn");
+        }else{
+            boxServiciosPrim.classList.remove("animate__zoomIn")
+            boxSecond.classList.remove("animate__zoomIn")
+            boxThrd.classList.remove("animate__zoomIn")
+            boxFour.classList.remove("animate__zoomIn")             
+        }        
+    })    
+}
+const animacionContacto =()=>{
+    window.addEventListener('scroll', ()=>{
+        const hacemos = document.querySelector("#hacemos")
+        const imagenContacto = document.querySelector(".imagen-contactaNos")
+        const infoContacto = document.querySelector(".info-contactaNos")
+       if(hacemos.getBoundingClientRect().top<0){
+        imagenContacto.classList.add("animate__rotateInDownLeft") 
+        infoContacto.classList.add("animate__rotateInUpRight")
+
+       }else{
+        imagenContacto.classList.remove("animate__rotateInDownLeft") 
+        infoContacto.classList.remove("animate__rotateInUpRight")
+
+       }        
+
+    })
+
+}
+
